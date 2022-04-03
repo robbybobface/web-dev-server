47543cb15112e53594ea58990a19f409199d13b6
Jarrett Anderson <jarrett2000@gmail.com>
2022-04-02 23:36:23 -0400

    first commit

---
+++ controllers/tuits-controller.js
@@ -0,0 +1,44 @@
+import posts from "./tuits/tuits.js";
+
+let tuits = posts;
+
+const createTuit = (req, res) => {
+    const newTuit = req.body;
+    newTuit._id = (new Date()).getTime() + '';
+    newTuit.postedBy = {
+        "username": "Jarrett"
+    };
+    newTuit.like = false;
+    newTuit.verified = false;
+    newTuit.handle = "robbybobface";
+    newTuit.time = "1 min";
+    newTuit.stats = {
+        "comments": "0",
+        "retuits": "0",
+        "likes": "0"
+    };
+    newTuit.logoImage = "https://pbs.twimg.com/profile_images/1502305122663157763/qbK0Ghl4_400x400.jpg";
+    tuits.unshift(newTuit);
+    res.json(newTuit);
+};
+const findAllTuits = (req, res) => {
+    res.json(tuits);
+};
+const updateTuit = (req, res) => {
+    const tuitIdToUpdate = req.params.tid;
+    const updatedTuit = req.body;
+    tuits = tuits.map(t => t._id === tuitIdToUpdate ? updatedTuit : t);
+    res.sendStatus(200);
+};
+const deleteTuit = (req, res) => {
+    const tuitIdToDelete = req.params.tid;
+    tuits = tuits.filter(t => t._id !== tuitIdToDelete);
+    res.sendStatus(200);
+};
+
+export default (app) => {
+    app.post('/api/tuits', createTuit);
+    app.get('/api/tuits', findAllTuits);
+    app.put('/api/tuits/:tid', updateTuit);
+    app.delete('/api/tuits/:tid', deleteTuit);
+}
---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,119 @@
+export default [
+    {
+        "_id": "123",
+        "topic": "Web Development",
+        "postedBy": {
+            "username": "ReactJS"
+        },
+        "liked": true,
+        "verified": false,
+        "handle": "ReactJS",
+        "time": "2h",
+        "title": "React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "attachments": {
+            "video": "unKvMC3Y1kI"
+        },
+        "logoImage": "https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK_400x400.png",
+        "avatarImage": "https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK_400x400.png",
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+            "likes": 345,
+            "dislikes": 123
+        }
+    },
+    {
+        "_id": "567",
+        "topic": "Space",
+        "postedBy": {
+            "username": "SpaceX"
+        },
+        "liked": false,
+        "verified": true,
+        "handle": "spacex",
+        "title": "",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "attachments": {
+            "image": "https://pbs.twimg.com/media/FOKKr9KVcAA91Lq?format=jpg&name=4096x4096"
+        },
+        "time": "last month",
+        "logoImage": "https://pbs.twimg.com/profile_images/1082744382585856001/rH_k3PtQ_400x400.jpg",
+        "avatarImage": "https://pbs.twimg.com/profile_images/1082744382585856001/rH_k3PtQ_400x400.jpg",
+        "tuits": "120K",
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+            "likes": 345,
+            "dislikes": 234
+        }
+    },
+    {
+        "_id": "234",
+        "topic": "Space",
+        "postedBy": {
+            "username": "Scott Manley"
+        },
+        "liked": true,
+        "verified": true,
+        "handle": "DJSnM",
+        "postedOn": "2020-12-10",
+        "time": "1 day",
+        "title": "JavaScript is programming language that can run on browsers as well as desktops",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "logoImage": "https://pbs.twimg.com/profile_images/1437977924657762312/EcS-uJ2k_400x400.jpg",
+        "avatarImage": "https://pbs.twimg.com/profile_images/1437977924657762312/EcS-uJ2k_400x400.jpg",
+        "tuits": "123K",
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+            "likes": 345,
+            "dislikes": 678
+        }
+    },
+    {
+        "_id": "345",
+        "topic": "Web Development",
+        "postedBy": {
+            "username": "jQuery"
+        },
+        "liked": false,
+        "verified": false,
+        "handle": "jQuery",
+        "title": "jQuery",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "time": "last week",
+        "logoImage": "https://pbs.twimg.com/profile_images/788014136575668224/lxEKmMnB_400x400.jpg",
+        "avatarImage": "https://pbs.twimg.com/profile_images/788014136575668224/lxEKmMnB_400x400.jpg",
+        "tuits": "122K",
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+            "likes": 345,
+            "dislikes": 789
+        }
+    },
+    {
+        "_id": "456",
+        "topic": "Web Development",
+        "postedBy": {
+            "username": "NodeJS"
+        },
+        "liked": true,
+        "verified": true,
+        "handle": "NodeJS",
+        "title": "",
+        "tuit": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        "time": "last month",
+        "logoImage": "https://pbs.twimg.com/profile_images/1262824892535373825/BiXDFDDp_400x400.jpg",
+        "avatarImage": "https://pbs.twimg.com/profile_images/1262824892535373825/BiXDFDDp_400x400.jpg",
+        "tuits": "120K",
+        "stats": {
+            "comments": 123,
+            "retuits": 234,
+            "likes": 345,
+            "dislikes": 123
+        }
+    }
+];
+
